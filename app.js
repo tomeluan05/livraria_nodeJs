70b4adad302bf9a0d3ef106797a6b66a8a6d5e7c
Tomé Luan <tomeluan05@gmail.com>
2022-11-18 15:56:32 -0300

    Config deploy

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 mongoose.connect("mongodb+srv://tomeluan05:tomeluan05@cluster0.m9ons.mongodb.net/?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true})
 
